af6ceab25eedd2dd932b61948060a0e04b031e8e
Sushil Kumar <sushildlh@gmail.com>
2017-06-09 13:55:00 +0530

    first commit

--- server.js
+++ server.js
@@ -24,29 +24,89 @@ if(!err) {
     console.log("Error connecting database ... nn");    
 }
 });
+app.post("/register",function(req,res){
+  var username=req.body.username;
+  var password=req.body.password;
+  connection.query("INSERT INTO userinfo (`username`, `password`) VALUES ('" + username + "','"+password+"')", function(err){
+    if(err){
+      res.send("Registeration failure");
+    }
+    else{
+      res.send("Registeration Success");
+    }});
+});
 app.post("/login",function(req,res){
   //login pageload
 var username=req.body.username;
  var password=req.body.password;
- sess=req.session; 
-     sess.username=username;
  console.log(username);
- connection.query('select * from userinfo where username= "'+username+'" and password="'+password+'"' ,function(err,result){
+ connection.query('select id from userinfo where username= "'+username+'" and password="'+password+'"' ,function(err,result){
   if(result.length==0){
     res.send("Authendication failure");
   }
   else{   
-   res.send("Login Success");
+     sess=req.session; 
+     sess.ID=result[0].id;
+     console.log(sess.ID);
+     connection.query('select * from folder where id="'+sess.ID+'"',function(err,result){
+      if(err){
+        res.send(err);
+      }
+      else
+      {
+        res.send({success:true,message:"Login success","data":result});
+      }
+     });
     }
  });
 });
+//------------------folderrename---------------------------
+app.post("/folderrename",function(req,res){
+  sess=req.session;
+  if(sess.ID){
+ var foldernewname=req.body.foldernewname;
+connection.query('UPDATE folder SET name = "'+foldernewname+'" WHERE id="'+sess.ID+'"',function(err,result){
+if(err){
+  res.send("Problem in Updating");
+}
+else{
+  res.send("Folder Name updated Succesfully");
+}
+});
+}
+else{
+  res.send("You Must login");
+}
+});
+//----------------------folder creation api.-----------------------------
+app.post("/folderadd",function(req,res){
+sess=req.session;
+if(sess.ID){
+  console.log(sess.id);
+var foldername=req.body.foldername;
+var created=new Date();
+created=created.getFullYear()+'-' + (created.getMonth()+1) + '-'+created.getDate();//prints expected format.
+console.log(created);
+connection.query("INSERT INTO folder (`id`, `name`,`created`) VALUES ('" + sess.ID + "', '" +foldername + "','" +created+ "')", function(err,result){
+if(err){
+  console.log(err);
+}
+else{
+  res.send("Folder is added");
+}
+});
+}
+else{
+  res.send("You Must Login");
+}
+});
 app.post("/addContent",function(req,res){
   sess=req.session;
-  if(sess.username){
+  if(sess.ID){
     var info=req.body.info;
     var link=req.body.link;
     var foldername=req.body.foldername;
-  connection.query("INSERT INTO textstore (`username`, `info`,`link`,`foldername`) VALUES ('" + sess.username + "', '" +info + "','" +link + "','"+foldername+"')", function(err){
+  connection.query("INSERT INTO textstore (`id`, `info`,`link`,`foldername`) VALUES ('" + sess.ID + "', '" +info + "','" +link + "','"+foldername+"')", function(err){
 if(err){
   res.send("Problem in storage");
 }
@@ -59,4 +119,36 @@ else{
     res.send("You Must Login");
   }
 });
+//---------------------search by the keyword-------------
+ app.post("/search",function(req,res){
+sess=req.session;
+if(sess.ID){
+var keyword=req.body.keyword;
+connection.query("select * from textstore where info like '%"+keyword+"%' and id="+sess.ID+"" ,function(err,result){
+if(err){
+  res.send(err);
+  console.log(err);
+}
+else{
+  res.send(result);
+}
+});
+}
+else{
+  res.send("You Must Login");
+}
+ }); 
+ app.get('/logout',function(req,res){
+  
+  req.session.destroy(function(err){
+    if(err){
+      console.log(err);
+    }
+    else
+    {
+      res.send('user logout');
+    }
+  });
+
+}); 
 app.listen(3000);
