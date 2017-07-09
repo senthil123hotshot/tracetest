e76b6b19cbb3df1af5880b9d4f01b9aa2cd0c34e
senthil <senthil@vectorx.co>
2017-07-09 13:33:46 +0530

    Create addme.sql

---
+++ addme.sql
@@ -0,0 +1,5 @@
+create database trace;
+use trace;
+create table userinfo(id int auto increment primary key,username varchar(100),password varchar(100));
+create table folder(id int auto increment primary key,name varchar(100),created date);
+create table textstore(id int auto increment primary key,info text,link text,foldername varchar(100));
