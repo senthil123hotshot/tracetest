8c3d466885b0a79537eba50d2d7cf0dded3792ab
senthil <senthil@vectorx.co>
2017-07-09 13:38:50 +0530

    Update addme.sql

--- addme.sql
+++ addme.sql
@@ -1,5 +1,6 @@
 create database trace;
 use trace;
 create table userinfo(id int auto increment primary key,username varchar(100),password varchar(100));
-create table folder(id int auto increment primary key,name varchar(100),created date);
-create table textstore(id int auto increment primary key,info text,link text,foldername varchar(100));
+create table folder(id int auto increment primary key,name varchar(100),created date,FOREIGN KEY (id) REFERENCES userinfo(id)
+);
+create table textstore(id int auto increment primary key,info text,link text,foldername varchar(100) ,FOREIGN KEY (id) REFERENCES userinfo(id));
